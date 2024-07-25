d12a053b311d68428b1953d4d1c8b7bf7ce0e9c3
lilu <lilu@bigdatacd.com>
2024-07-25 13:53:04 +0800

    性能优化

--- src/App.vue
+++ src/App.vue
@@ -12,28 +12,16 @@
 <script setup>
 import { ref, onMounted, nextTick, watch } from "vue";
 const _elTableRef = ref();
-const tableData = ref([
-    {
-        date: "2016-05-03",
-        name: "Tom",
-        address: "No. 189, Grove St, Los Angeles",
-    },
-    {
-        date: "2016-05-02",
-        name: "Tom",
-        address: "No. 189, Grove St, Los Angeles",
-    },
-    {
-        date: "2016-05-04",
-        name: "Tom",
-        address: "No. 189, Grove St, Los Angeles",
-    },
-    {
-        date: "2016-05-01",
-        name: "Tom",
-        address: "No. 189, Grove St, Los Angeles",
-    },
-]);
+const tableData = ref(
+    Array.from({ length: 10 }).map((_, index) => {
+        return {
+            date: "2016-05-03",
+            name: "Tom",
+            address: "No. 189, Grove St, Los Angeles",
+            index,
+        };
+    })
+);
 function doAutoSize(column) {
     if (!column) {
         return;
@@ -41,15 +29,40 @@ function doAutoSize(column) {
     //拿到该列单元格
     const tds = _elTableRef.value.$el.querySelectorAll(`.${column.id}>.cell`);
     //拿到单元格内容宽度
-    const tdWidths = [...tds].map((element) => {
-        element.classList.add("testElementSize");
-        const width =
-            element.getBoundingClientRect().width || element.scrollWidth;
-        element.classList.remove("testElementSize");
-        //这里+2是因为element的表格可能有左右边框两个像素
-        console.log("width", width);
-        return Math.ceil(width) + 2;
-    });
+    //console.time("testElementSize1");
+    // 此写法布局抖动，1000行数据需要2秒钟
+    // let tdWidths = [...tds].map((element) => {
+    //     element.classList.add("testElementSize");
+    //     const width =
+    //         element.getBoundingClientRect().width || element.scrollWidth;
+    //     element.classList.remove("testElementSize");
+    //     //这里+2是因为element的表格可能有左右边框两个像素
+    //     return Math.ceil(width) + 2;
+    // });
+    //console.timeEnd("testElementSize1");//需要1-2秒
+
+    //优化写法，1000行数据只需要几十毫秒
+    //先给全部的单元格设置testElementSize类
+    //然后再获取宽度,此时没有布局抖动,页面不会重绘redraw（指挥redraw一次）
+    //最有移除掉
+    console.time("testElementSize2");
+    const tdWidths = [...tds]
+        .map((element) => {
+            element.classList.add("testElementSize");
+            return element;
+        })
+        .map((element) => {
+            const width =
+                element.getBoundingClientRect().width || element.scrollWidth;
+            //这里+2是因为element的表格可能有左右边框两个像素
+            return {element, width:Math.ceil(width) + 2};
+        })
+        .map(({element, width}) => {
+            element.classList.remove("testElementSize");
+            return width;
+        });
+
+    console.timeEnd("testElementSize2");//大约20-30毫秒
     //最大的单元格宽度
     let maxWidth = Math.max(...tdWidths);
     //设置该列宽度并更新布局
